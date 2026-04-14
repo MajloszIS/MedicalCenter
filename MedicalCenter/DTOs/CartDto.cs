9bdfe494de553270af6769817444f47c67b16bff
Jakub Komorowski <91924@student.pb.edu.pl>
2026-04-14 22:43:52 +0200

    Apoteka Dtos

---
+++ MedicalCenter/DTOs/CartDto.cs
@@ -0,0 +1,8 @@
+﻿namespace MedicalCenter.DTOs
+{
+    public class CartDto
+    {
+        public List<CartItemDto> Items { get; set; } = new();
+        public decimal TotalCartPrice => Items.Sum(i => i.TotalItemPrice);
+    }
+}
