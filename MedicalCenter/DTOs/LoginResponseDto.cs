38e2e4124b61df1ccc79a453e610ce57a9a723c7
Majlosz <psp13msz@gmail.com>
2026-05-16 13:05:27 +0200

    Add Api Auth Configuration with JWT

---
+++ MedicalCenter/DTOs/LoginResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MedicalCenter.DTOs
+{
+    public class LoginResponseDto
+    {
+        public string Token { get; set; }
+        public DateTime ExpiresAt { get; set; }
+        public string FullName { get; set; }
+        public string RoleName { get; set; }
+    }
+}
