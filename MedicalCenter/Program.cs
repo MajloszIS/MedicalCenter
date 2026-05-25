1c1c58ec082bbe449e4d33f9a77bf8b74391faf8
Majlosz <psp13msz@gmail.com>
2026-05-26 00:21:29 +0200

    Update Stripe domain url, Google url and profile pictures upload path

--- MedicalCenter/Program.cs
+++ MedicalCenter/Program.cs
@@ -140,11 +140,8 @@ QuestPDF.Settings.License = QuestPDF.Infrastructure.LicenseType.Community;
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
